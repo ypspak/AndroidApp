910194b7757ff8cad41fb3d97fd6f6faa51e19ce
marvin5064 <marvin5064@gmail.com>
2015-10-23 14:17:23 +0800

    Allows Room name, Key of corresponding Qeustion & firebasedUrl pass to the replyActivity

--- app/src/main/java/hk/ust/cse/hunkim/questionroom/ReplyActivity.java
+++ app/src/main/java/hk/ust/cse/hunkim/questionroom/ReplyActivity.java
@@ -6,13 +6,19 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import com.firebase.client.Firebase;
+
 /**
  * Created by CAI on 21/10/2015.
  */
 public class ReplyActivity extends ListActivity {
+    private static final String FIREBASE_URL = "https://cmkquestionsdb.firebaseio.com/";
 
     private TextView questionContent;
-    private String questionTitle;
+    private String key;
+    private String roomName;
+    private Firebase mFirebaseRef;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reply);
@@ -21,9 +27,13 @@ public class ReplyActivity extends ListActivity {
         assert (intent != null);
 
         //currently just for testing that I am entered the replying room corresponding to the question
-        questionTitle = intent.getStringExtra(QuestionListAdapter.REPLIED_QEUSTION);
+        key = intent.getStringExtra(QuestionListAdapter.REPLIED_QEUSTION);
+        roomName = intent.getStringExtra(QuestionListAdapter.ROOM_NAME);
+        mFirebaseRef = new Firebase(FIREBASE_URL).child(roomName).child("questions").child(key);
         questionContent = (TextView) findViewById(R.id.Question);
-        questionContent.setText(questionTitle);
+
+        questionContent.setText(key + "," + roomName);
+
 
 
 
