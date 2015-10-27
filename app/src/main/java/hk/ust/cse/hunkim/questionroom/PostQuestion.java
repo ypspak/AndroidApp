db01c26a0bd94a3c031a9e0244fa0c5e938f0ea6
marvin5064 <marvin5064@gmail.com>
2015-10-27 13:04:02 +0800

    Initiate new files for popbox which is used for input the question title and content

---
+++ app/src/main/java/hk/ust/cse/hunkim/questionroom/PostQuestion.java
@@ -0,0 +1,17 @@
+package hk.ust.cse.hunkim.questionroom;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by CAI on 27/10/2015.
+ */
+public class PostQuestion extends Activity {
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reply);
+
+    }
+}
