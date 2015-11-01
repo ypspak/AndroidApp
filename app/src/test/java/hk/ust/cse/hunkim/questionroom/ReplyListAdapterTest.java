ce382de446355556636aef706b08420c3182607a
marvin5064 <marvin5064@gmail.com>
2015-11-02 00:03:01 +0800

    testing cases initiation

---
+++ app/src/test/java/hk/ust/cse/hunkim/questionroom/ReplyListAdapterTest.java
@@ -0,0 +1,21 @@
+package hk.ust.cse.hunkim.questionroom;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by CAI on 2/11/2015.
+ */
+public class ReplyListAdapterTest extends TestCase {
+
+    public void testPopulateView() throws Exception {
+
+    }
+
+    public void testSortModels() throws Exception {
+
+    }
+
+    public void testSetKey() throws Exception {
+
+    }
+}
