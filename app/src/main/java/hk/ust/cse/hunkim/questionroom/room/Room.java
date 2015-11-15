ad62a098d11e1559f523c8429083cc1a2b77f4f3
marvin5064 <marvin5064@gmail.com>
2015-11-15 14:45:54 +0800

    finished room creation

--- app/src/main/java/hk/ust/cse/hunkim/questionroom/room/Room.java
+++ app/src/main/java/hk/ust/cse/hunkim/questionroom/room/Room.java
@@ -4,20 +4,18 @@ package hk.ust.cse.hunkim.questionroom.room;
  * Created by CAI on 15/11/2015.
  */
 public class Room {
-    private String name;
+    private String roomName;
     private boolean isPrivate;
     private String password;
 
     private Room(){}
-    public Room(String name, boolean isPrivate, String password){
-        this.name = name;
+    public Room(String roomName, boolean isPrivate, String password){
+        this.roomName = roomName;
         this.isPrivate = isPrivate;
         this.password = password;
     }
 
-    public boolean checkPassword(String password){
-        return this.password.equals(password);
-    }
-    public String getName(){ return name;}
+    public String getRoomName(){return roomName;}
     public boolean getIfPrivate(){ return isPrivate;}
+    public String getPassword(){return password;}
 }
