f2e504c7dd89b17bd50511961563e5438d635b4b
Peter Yeung <ypspak@gmail.com>
2015-11-16 21:44:35 +0800

    Fix question description at ReplyActivity will not handle lt and gt symbol.

--- app/src/main/java/hk/ust/cse/hunkim/questionroom/ReplyActivity.java
+++ app/src/main/java/hk/ust/cse/hunkim/questionroom/ReplyActivity.java
@@ -6,6 +6,7 @@ import android.database.DataSetObserver;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.text.Html;
 import android.text.TextUtils;
 import android.text.format.DateUtils;
 import android.util.Log;
@@ -321,7 +322,7 @@ public class ReplyActivity extends ListActivity {
                             }
                             else {
                                 String tempStr = (String) dataSnapshot.getValue();
-                                textView.setText("" + tempStr);
+                                textView.setText(Html.fromHtml("" + tempStr));
                             }
                         }
                     }
