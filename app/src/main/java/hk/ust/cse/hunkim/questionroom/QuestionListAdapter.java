1fd9a7b78680cc9d0ff862960880e6be37bedf76
marvin5064 <marvin5064@gmail.com>
2015-11-16 22:01:47 +0800

    fixed the sequence of sort by new

--- app/src/main/java/hk/ust/cse/hunkim/questionroom/QuestionListAdapter.java
+++ app/src/main/java/hk/ust/cse/hunkim/questionroom/QuestionListAdapter.java
@@ -280,26 +280,26 @@ public class QuestionListAdapter extends FirebaseListAdapter<Question> {
     private int difCompareWay(Question question1, Question question2){
         int method = this.getSortMethod();
         if(method==0){ //newest
-            return question1.getTimestamp() > question2.getTimestamp() ? 1 : -1;
+            return question1.getTimestamp() < question2.getTimestamp() ? 1 : -1;
         }else if(method==1){ //hot
             if ((question1.getTimestamp()+7200000*(3*question1.getLike()+2*question1.getReplies()+question1.getDislike()))
                     == (question2.getTimestamp()+7200000*(3*question2.getLike()+2*question2.getReplies()+question2.getDislike()))) {
-                return question1.getTimestamp() > question2.getTimestamp() ? 1 : -1;
+                return question1.getTimestamp() < question2.getTimestamp() ? 1 : -1;
             }
             return (question1.getTimestamp()+7200000*(3*question1.getLike()+2*question1.getReplies()+question1.getDislike()))
                     < (question2.getTimestamp()+7200000*(3*question2.getLike()+2*question2.getReplies()+question2.getDislike()))? 1:-1;
         }else if(method == 2) {//like
             if (question1.getLike() == question2.getLike()) {
-                return question1.getTimestamp() > question2.getTimestamp() ? 1 : -1;
+                return question1.getTimestamp() < question2.getTimestamp() ? 1 : -1;
             }
             return question2.getLike() - question1.getLike();
         }else if(method == 3){//dislike
             if (question1.getDislike() == question2.getDislike()) {
-                return question1.getTimestamp() > question2.getTimestamp() ? 1 : -1;
+                return question1.getTimestamp() < question2.getTimestamp() ? 1 : -1;
             }
             return question2.getDislike() - question1.getDislike();
         }else if (method == 4){//latest reply
-            return question1.getLastTimestamp() > question2.getLastTimestamp() ? 1 : -1;
+            return question1.getLastTimestamp() < question2.getLastTimestamp() ? 1 : -1;
         }
         return 0;
     }
